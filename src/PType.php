ab6f42076569e707bb4c1d46139f365816f1bb9c
Daniel Pozzi <bonndan76@googlemail.com>
2012-11-20 21:45:31 +0100

    moved comparison constants

--- src/PType.php
+++ src/PType.php
@@ -1,9 +1,36 @@
 <?php
 /**
- * PType - base class for all typess
+ * PType - base class for all types
  */
 abstract class PType
 {
+    /**
+     * Returns value used by compareTo().
+     *
+     * Indicates that this string is less than the compared one.
+     *
+     * @var integer
+     */
+    const COMPARE_LESS_THAN_OTHER = -1;
+    
+    /**
+     * Returns value used by compareTo().
+     *
+     * Indicates that this string equals the compared one.
+     *
+     * @var integer
+     */
+    const COMPARE_EQUALS_OTHER = 0;
+    
+    /**
+     * Returns value used by compareTo().
+     *
+     * Indicates that this string is greater than the compared one.
+     *
+     * @var integer
+     */
+    const COMPARE_GREATER_THAN_OTHER = 1;
+    
     /**
      * the "native" php value
      * @var mixed
